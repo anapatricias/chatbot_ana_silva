cfe492aae67c66c27e96679c350de1e44a5b3265
anapatricias <anapatriciaportelinha@gmail.com>
2025-07-03 11:43:09 +0100

    revisão_final2

--- app.py
+++ app.py
@@ -25,24 +25,28 @@ def obter_resposta(texto: str) -> str:
          ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
         'como estás': 'Estou bem, obrigado!',
         ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
-        #novos comentários
-          'qual é o teu nome?':'O meu nome é Bot!', 
-          'quantos anos tens?': 'Sou muito jovem',
-          'como está a meteorologia em Portugal nos últimos dias?':'Está muito calor!',
-          'preferes este tempo ou o inverno?':'gosto mais deste tempo, mas é demasiado calor',
-          'como gostas de passar o tempo?':'gosto muito de ler e aprender coisas novas',
-          'que tipo de livros gostas?':'um pouco de tudo! mas os meus favoritos são romances históricos',
-          'qual o último livro que leste?':'No tempos das cerejas, sobre uma Lisboa após a segunda guerra mundial',
-          'e o próximo?':'vou começar um sobre a Ucrânia no tempo da União Soviética',
-          'como é bom viajarmos para outros países e tempos sem sair do lugar, não achas?':'sim! melhor sensação do mundo',
-          'espero que continues sempre a ler!':'eu também!obrigado',
+
+    # novos comentários 
+    ('qual é o teu nome', 'qual o teu nome', 'como te chamas'): 'O meu nome é Bot!',
+    ('quantos anos tens', 'qual a tua idade'): 'Sou muito jovem',
+    ('meteorologia em portugal', 'tempo em portugal', 'clima em portugal'): 'Está muito calor!',
+    ('preferes este tempo ou o inverno', 'gosta mais do verão ou do inverno'): 'Gosto mais deste tempo, mas é demasiado calor',
+    ('como gostas de passar o tempo', 'o que gostas de fazer'): 'Gosto muito de ler e aprender coisas novas',
+    ('que tipo de livros gostas', 'quais os teus livros favoritos'): 'Um pouco de tudo! Mas os meus favoritos são romances históricos',
+    ('qual o último livro que leste', 'último livro que leste'): 'No tempos das cerejas, sobre uma Lisboa após a segunda guerra mundial',
+    ('qual é o próximo livro', 'próximo livro que vais ler'): 'Vou começar um sobre a Ucrânia no tempo da União Soviética',
+    ('como é bom viajar sem sair do lugar', 'viajar sem sair do lugar'): 'Sim! Melhor sensação do mundo',
+    ('espero que continues sempre a ler', 'continue a ler'): 'Eu também! Obrigado',
+}
+
           
-    }
 
     for chave, resposta in respostas.items():
          if isinstance(chave, tuple):
-            if comando in chave:
-                 return resposta
+             for frase in chave:
+                 if frase in comando:
+                     return resposta
+            
     else: 
          if chave in comando:
           return resposta
